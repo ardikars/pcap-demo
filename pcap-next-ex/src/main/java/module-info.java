2b2f79500e788eeacf97f10a62a2d88fad99f816
Ardika Rommy Sanjaya <contact@ardikars.com>
2021-12-04 13:29:16 +0700

    Add logback

--- pcap-next-ex/src/main/java/module-info.java
+++ pcap-next-ex/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module pcap.next {
-    requires pcap.spi;
-    requires pcap.codec;
-    requires pcap.common;
-    requires org.apache.logging.log4j;
+  requires pcap.spi;
+  requires pcap.codec;
+  requires pcap.common;
+  requires org.apache.logging.log4j;
+// requires org.slf4j;
 }
